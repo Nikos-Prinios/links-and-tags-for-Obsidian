db3ee53638caec8464de2aa61701fb18a67f001e
Nikos <nikos@habanita.org>
2024-03-15 07:42:09 +0100

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,25 @@
+tags = [
+    "Brainstorming", "Pensée latérale", "Résolution de problèmes", "Innovation",
+    "Imagination", "Idéation", "Pensée divergente", "Pensée convergente",
+    "Créativité fluide", "Flexibilité cognitive", "Originalité", "Heuristique",
+    "Intuition", "Métaphore", "Analogie", "Synthèse", "Incubation", "Curiosité",
+    "Exploration", "Expérimentation", "Visualisation", "Mind Mapping", "Serendipity",
+    "Brainwriting", "SCAMPER", "TRIZ", "Six Chapeaux de réflexion", "Pensée critique",
+    "Flow", "Éveil", "Insight", "Challenge", "Collaboration", "Disruption", "disruptif", "Prototype",
+    "Iteration", "Échec productif", "Ambiguïté", "Risque", "Perspective", "Empathie",
+    "Storytelling", "Design Thinking", "Biomimétisme", "Morphologique Analysis",
+    "Synectics", "Intelligence créative", "Pensée systémique", "Divergence", "Reframing",
+    "Génération d'idées", "Évaluation critique", "Modèles mentaux", "Résonance",
+    "Improvisation", "Intégration", "Transdisciplinarité", "Connaissance tacite",
+    "Transfert de connaissances", "Plasticité cérébrale", "Épistémologie", "Paradigme",
+    "Saut quantique", "Détournement", "Cross-pollination", "Conceptualisation",
+    "Analogie structurelle", "Pensée abstraite", "Faisabilité", "Viabilité", "Réalisation",
+    "Transformation", "Quête de sens", "Énigme", "Paradoxe", "Non-linéarité",
+    "Multidimensionnalité", "Génération Y/Z", "Discontinuité", "Fusion", "Éclatement",
+    "Émergence", "Hybridation", "Confluence", "Polymorphisme", "Modularité", "Réversibilité",
+    "Scalabilité", "Aléatoire", "Chaos", "Entropie", "Fractal", "Hologramme", "Décentrement",
+    "Globalité", "Interconnexion", "Multiculturalisme", "Universalité", "Idiosyncrasie",
+    "Iconoclasme"
+]
+
+# Here, ask GPT to create a relevant list for you
